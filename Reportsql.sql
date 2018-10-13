6e7d6d93d0eb4816143ed9b7b48e0853c0688f35
N.Kranthi Kumar <kranthi.n13@iiits.in>
2018-10-13 10:39:30 +0530

    Updated Report Sql
    Added inner Join Query

--- Reportsql.sql
+++ Reportsql.sql
@@ -1,16 +1,32 @@
--- Query for Getting the Company Wise Period Wise Sum of KPis Data of cla_point
+-- Query for Getting the Company Wise Period Wise Sum of KPis Data of Cla Point(Report For FY)
 
-SELECT pdca_kpi.companyId,pdca_cla_kpi.periodId,sum(pdca_cla_kpi.claPoint) as sum_cla_point
-FROM pdca.pdca_kpi,pdca.pdca_cla_kpi 
-where pdca_kpi.kpiId = pdca_cla_kpi.kpiId and year = 2017 and periodId in (3,4,5)
-group by pdca_kpi.companyId,pdca_cla_kpi.periodId order by pdca_kpi.companyId ASC;
+SELECT k.companyId AS cid,ck.periodId AS pid,SUM(ck.claPoint) AS sum_cla_point
+FROM pdca_kpi k 
+INNER JOIN pdca_cla_kpi ck
+ON k.kpiId = ck.kpiId
+WHERE k.year = 2017 AND ck.periodId IN (3,4,5)
+GROUP BY k.companyId,ck.periodId 
+ORDER BY k.companyId ASC;
 
--- Query for Getting the Company Period year Based KPis Data of cla_point of Trading Profit
+-- Query for Getting the Company Period year Based KPis Data of Achivement of Trading Profit(Report For FY)
 
-SELECT pdca_kpi.companyId,pdca_cla_kpi.periodId,pdca_cla_kpi.claPoint FROM pdca.pdca_kpi,pdca.pdca_cla_kpi 
-where pdca_kpi.kpiId = pdca_cla_kpi.kpiId and year = 2017 and periodId in (3,4,5) and pdca_kpi.itemDescriptionName = 1
-group by pdca_kpi.companyId,pdca_cla_kpi.periodId order by pdca_kpi.companyId ASC;
+SELECT k.companyId AS cid,ck.periodId AS pid,ck.achivement 
+FROM pdca_kpi k 
+INNER JOIN pdca_cla_kpi ck
+ON k.kpiId = ck.kpiId
+WHERE k.year = 2017 AND ck.periodId IN (3,4,5) AND k.itemDescriptionName = 1
+GROUP BY k.companyId,ck.periodId 
+ORDER BY k.companyId ASC;
 
+-- Query for Getting the Company Period year Based KPis Data of OL of Trading Profit(Report For CP)
+
+SELECT k.companyId AS cid,ck.periodId AS pid,ck.ol
+FROM pdca_kpi k 
+INNER JOIN pdca_cla_kpi ck
+ON k.kpiId = ck.kpiId
+WHERE k.year = 2017 AND ck.periodId IN (3,4,5) AND k.itemDescriptionName = 1
+GROUP BY k.companyId,ck.periodId 
+ORDER BY k.companyId ASC;
 
 
 
